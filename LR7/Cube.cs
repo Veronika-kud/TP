57dd152388a5eb5872d8200693084277447ad0f8
Veronika-kud <71777476+Veronika-kud@users.noreply.github.com>
2020-10-27 15:29:03 +0300

    LR7
    Лабораторная работа №6
    25.10.2020
    Технологии Программирования

---
+++ LR7/Cube.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR7
+{
+    class Cube
+    {
+        private double a;
+
+        public Cube() { }
+
+        public Cube(double pA) 
+        { 
+            a = pA;
+        }
+
+        public void Load() 
+        { 
+            a = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static Cube CreateCubeFromFile()
+        {
+            double aa = Convert.ToDouble(Console.ReadLine());
+            return new Cube(aa);
+        }
+
+        public double GetV() { return a * a * 6; }
+
+        public double GetS() { return a * a * a; }
+
+        public void Info()
+        {
+            String str =
+
+                "\n ____________________ \n" +
+                "\n|                    |  \n" +
+                "\n|        КУБ         | \n" +
+                "\n|                    | \n" +
+                "\n|____________________| \n";
+
+            Console.WriteLine(str);
+            Console.WriteLine(string.Format("сторона куба: " + "A = {0:.0.00}", a));
+            Console.WriteLine(string.Format("Объем: " + "V= {0:.0.00}", GetV()));
+            Console.WriteLine(string.Format("Площадь поверхности: " + "S = {0:.0.00}", GetS()));
+            Console.WriteLine();
+        }
+
+        public void Info(ConsoleColor fg, ConsoleColor bgc)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgc;
+            Console.Clear();
+            Info();
+        }
+    }
+}
---
+++ LR7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"Cube_output.txt");
+            var new_in = new StreamReader(@"Cube_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Cube c1, c2;
+#if DEBUG
+            c1 = new Cube(19);
+            c1.Info(ConsoleColor.Red, ConsoleColor.White);
+#endif
+
+#if !DEBUG
+            c1 = Cube.CreateCubeFromFile();
+            c1.Info();
+            c2 = new Cube(5);
+            c2.Info();
+#endif
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+
+#if DEBUG
+            Console.ReadKey();
+#endif
+        }
+    }
+}
