988e51c4fa239d31621caa47e4f2102c0944263e
Veronika-kud <71777476+Veronika-kud@users.noreply.github.com>
2020-12-24 23:48:02 +0300

    LR11
    Лабораторная Работа №11
    Технологии Программирования

---
+++ LR11/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace лаб11
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            StreamReader f_in = new StreamReader("lr11_07.csv");
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            var new_out = new StreamWriter(@"lr11.txt");
+            Console.SetOut(new_out);
+#endif
+
+            List<SiteLr> all = new List<SiteLr>();
+
+            try
+            {
+                String line = f_in.ReadLine();
+                while ((line = f_in.ReadLine()) != null)
+                {
+                    all.Add(SiteLr.Create(line));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Всего пользователей: {0}", all.Count);
+            foreach (var p in all)
+                Console.WriteLine(p);
+
+
+
+            Console.WriteLine("******************************* Задание 1 *******************************");
+            Console.WriteLine("******* Количество сотрудников без логина *******\n");
+
+            int amount = all.FindAll(p => p.Login == "").ToList().Count;
+            Console.WriteLine("Количество сотрудников без логина: {0}", amount);
+
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("******************************* Задание 2 *******************************");
+            Console.WriteLine("*******  Средняя заработная плата по каждому из 4-х отделов *******\n");
+
+            float averageA = (from p in all
+                                       where (p.DepartmentName == Department.A)
+                                       select p.Salary).Average();
+            float averageB = (from p in all
+                              where (p.DepartmentName == Department.B)
+                              select p.Salary).Average();
+            float averageC = (from p in all
+                              where (p.DepartmentName == Department.C)
+                              select p.Salary).Average();
+            float averageD = (from p in all
+                              where (p.DepartmentName == Department.D)
+                              select p.Salary).Average();
+            Console.WriteLine(String.Format("Средння з/п в отделе А: ${0}", averageA));
+            Console.WriteLine(String.Format("Средння з/п в отделе B: ${0}", averageB));
+            Console.WriteLine(String.Format("Средння з/п в отделе C: ${0}", averageC));
+            Console.WriteLine(String.Format("Средння з/п в отделе D: ${0}", averageD));
+
+            Console.WriteLine("\n\n");
+
+
+            Console.WriteLine("******************************* Задание 3 *******************************");
+            Console.WriteLine("******* Максимальная з/п сотрудников у которых есть собака или кошка *******\n");
+            float SalaryAMaxC = (from p in all
+                               where (p.DepartmentName == Department.A) && p.HasCat
+                               select p.Salary).Max();
+            float SalaryBMaxC = (from p in all
+                                 where (p.DepartmentName == Department.B) && p.HasCat
+                                 select p.Salary).Max();
+            float SalaryCMaxC = (from p in all
+                                 where (p.DepartmentName == Department.C) && p.HasCat
+                                 select p.Salary).Max();
+            float SalaryDMaxC = (from p in all
+                                 where (p.DepartmentName == Department.D) && p.HasCat
+                                 select p.Salary).Max();
+            float SalaryAMaxD = (from p in all
+                                 where (p.DepartmentName == Department.A) && p.HasDog
+                                 select p.Salary).Max();
+            float SalaryBMaxD = (from p in all
+                                 where (p.DepartmentName == Department.B) && p.HasDog
+                                 select p.Salary).Max();
+            float SalaryCMaxD = (from p in all
+                                 where (p.DepartmentName == Department.C) && p.HasDog
+                                 select p.Salary).Max();
+            float SalaryDMaxD = (from p in all
+                                 where (p.DepartmentName == Department.D) && p.HasDog
+                                 select p.Salary).Max();
+
+
+            float[] max = new float [8];
+            max[0] = SalaryAMaxC;
+            max[1] = SalaryBMaxC;
+            max[2] = SalaryCMaxC;
+            max[3] = SalaryDMaxC;
+            max[4] = SalaryAMaxD;
+            max[5] = SalaryBMaxD;
+            max[6] = SalaryCMaxD;
+            max[7] = SalaryDMaxD;
+
+            float MaxSalary = max.Max();
+            Console.WriteLine(" Максимальная з/п сотрудников у которых есть собака или кошка: " + MaxSalary + "$");
+
+            Console.WriteLine("\n\n\n");
+
+
+            Console.WriteLine("******************************* Задание 4 *******************************");
+            Console.WriteLine("******* Средняя з/п у сотрудников с оружием и без. *******\n");
+
+            float AverageAMaxC = (from p in all
+                                 where (p.DepartmentName == Department.A) && p.HasGun
+                                 select p.Salary).Average();
+            float AverageBMaxC = (from p in all
+                                 where (p.DepartmentName == Department.B) && p.HasGun
+                                 select p.Salary).Average();
+            float AverageCMaxC = (from p in all
+                                 where (p.DepartmentName == Department.C) && p.HasGun
+                                 select p.Salary).Average();
+            float AverageDMaxC = (from p in all
+                                 where (p.DepartmentName == Department.D) && p.HasGun
+                                 select p.Salary).Average();
+            float AverageAMaxD = (from p in all
+                                 where (p.DepartmentName == Department.A) && !p.HasGun
+                                 select p.Salary).Average();
+            float AverageBMaxD = (from p in all
+                                 where (p.DepartmentName == Department.B) && !p.HasGun
+                                 select p.Salary).Average();
+            float AverageCMaxD = (from p in all
+                                 where (p.DepartmentName == Department.C) && !p.HasGun
+                                  select p.Salary).Average();
+            float AverageDMaxD = (from p in all
+                                 where (p.DepartmentName == Department.D) && !p.HasGun
+                                  select p.Salary).Average();
+;
+            Console.WriteLine(String.Format("Средння зарплата отдела А с оружием: ${0}", AverageAMaxC));
+            Console.WriteLine(String.Format("Средння зарплата отдела B с оружием: ${0}", AverageBMaxC));
+            Console.WriteLine(String.Format("Средння зарплата отдела C с оружием: ${0}", AverageCMaxC));
+            Console.WriteLine(String.Format("Средння зарплата отдела D с оружием: ${0}", AverageDMaxC));
+            Console.WriteLine(String.Format("Средння зарплата отдела А без оружия: ${0}", AverageAMaxD));
+            Console.WriteLine(String.Format("Средння зарплата отдела B без оружия: ${0}", AverageBMaxD));
+            Console.WriteLine(String.Format("Средння зарплата отдела C без оружия: ${0}", AverageCMaxD));
+            Console.WriteLine(String.Format("Средння зарплата отдела D без оружия: ${0}", AverageDMaxD));
+
+#if !DEBUG
+            Console.SetOut(save_out);
+            new_out.Close();
+#endif
+        }
+    }
+}
---
+++ LR11/Site.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace лаб11
+{
+    enum Department
+    {
+        A,
+        B,
+        C,
+        D,
+
+    }
+
+    class SiteLr
+    {
+        String ID { get; set; }
+        public String Login { get; set; }
+        String Password { get; set; }
+        public Department DepartmentName { get; set; }
+        public float Salary { get; set; }
+        public Boolean HasCat { get; set; }
+        public Boolean HasDog { get; set; }
+        public Boolean HasGun { get; set; }
+
+        public static SiteLr Create(String str)
+        {
+            SiteLr p = new SiteLr();
+            //string[] e = str.Split(',');
+            string[] e = Regex.Split(str, ",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+            p.ID = e[0].Trim();
+            p.Login = e[1].Trim();
+            p.Password = e[2].Trim();
+            String tmp = e[4].Trim();
+            if (tmp == "A")
+                p.DepartmentName = Department.A;
+            else if (tmp == "B")
+                p.DepartmentName = Department.B;
+            else if (tmp == "C")
+                p.DepartmentName = Department.C;
+            else
+                p.DepartmentName = Department.D;
+
+            p.Salary = Convert.ToSingle(e[3].TrimStart('$').Replace('.', ','));
+            p.HasCat = Convert.ToBoolean(e[5].Trim());
+            p.HasDog = Convert.ToBoolean(e[6].Trim());
+            p.HasGun = Convert.ToBoolean(e[7].Trim());
+
+            return p;
+        }
+
+        public static String DepartmentToStr(Department c)
+        {
+            if (c == Department.A) return "A";
+            else if (c == Department.B) return "B";
+            else if (c == Department.C) return "C";
+            else return "D";
+        }
+
+        public static String HasCatSt(Boolean d)
+        {
+            if (d == true) return "Есть кошка";
+            else return "Нет кошки";
+        }
+
+        public static String HasDogSt(Boolean r)
+        {
+            if (r == true) return "Есть собака";
+            else return "Нет собака";
+        }
+
+        public static String HasGunSt(Boolean a)
+        {
+            if (a == true) return "Есть оружие";
+            else return "Нет оружия";
+        }
+
+        public override string ToString()
+        {
+            String s = string.Format(
+                "*******************************************************\n" +
+                "ID: {0} \n" +
+                "Логин: {1}\n" +
+                "Пароль: {2}\n" +
+                "Зарплата: {4} $\n" +
+                "Отделение: {3}\n" +
+                "Наличие кошки: {5}\n" +
+                "Наличие собаки: {6}\n" +
+                "Наличие оружия: {7}\n" +
+                "*******************************************************\n",
+                ID, Login, Password, DepartmentName, Salary, HasCatSt(HasCat), HasDogSt(HasDog), HasGunSt(HasGun));
+            return s; 
+        }
+    }
+}
